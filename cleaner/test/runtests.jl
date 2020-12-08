564c2efca524770746ad081d005c11215d17884d
Jack Hanslope <code@jackhanslope.com>
2020-12-08 22:21:51 +0000

    Fix typo in tests
    I'd changed "clean" to "cleaner" but forgot to fix it everywhere.

--- cleaner/test/runtests.jl
+++ cleaner/test/runtests.jl
@@ -1,20 +1,20 @@
 using cleaner
 using Test
 
-@testset "clean.jl" begin
-    articles = clean.articles_from_xml("data/articles.xml", "data/truth.xml")
-    @test articles[3] == clean.Article(678787, "Jack is good", "As the title says.", true)
+@testset "cleaner.jl" begin
+    articles = cleaner.articles_from_xml("data/articles.xml", "data/truth.xml")
+    @test articles[3] == cleaner.Article(678787, "Jack is good", "As the title says.", true)
     @test articles[1].id == 643
     @test !articles[1].label
     @test articles[2].title ==
           "Hollywood Actors Who Condemn Trump but Were Silent on Weinstein"
 
     open("data/clean.json", "r") do io
-        @test readlines(io)[1] == clean.articles_to_json(articles)
+        @test readlines(io)[1] == cleaner.articles_to_json(articles)
     end
 
     open("data/clean.json", "r") do io
-        @test readlines(io)[1] == clean.run("data/articles.xml", "data/truth.xml")
+        @test readlines(io)[1] == cleaner.run("data/articles.xml", "data/truth.xml")
     end
 
 end # testcase
