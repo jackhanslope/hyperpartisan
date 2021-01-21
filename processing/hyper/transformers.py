15ce9e4f1563c9bf92c2d222f0a48287c21d2730
Jack Hanslope <code@jackhanslope.com>
2021-01-21 18:25:37 +0000

    Make docstrings one line

--- processing/hyper/transformers.py
+++ processing/hyper/transformers.py
@@ -1,6 +1,4 @@
-"""
-A collection of sklearn compliant transformers
-"""
+""" A collection of sklearn compliant transformers """
 from typing import Iterable
 
 import numpy as np
@@ -10,31 +8,23 @@ from sklearn.base import BaseEstimator, TransformerMixin
 
 
 class SpacyTransformer(BaseEstimator, TransformerMixin):
-    """
-    A transformer based off spacy models
-    """
+    """ A transformer based off spacy models """
 
     def __init__(self, model, model_name):
         self.model = model
         self.model_name = model_name
 
     def transform_sentence(self, text: str) -> np.array:
-        """
-        Return the vector representation of `text`
-        """
+        """ Return the vector representation of `text` """
         return self.model(text).vector
 
     def fit(self, features, target=None):
-        """
-        Required function
-        """
+        """ Required function """
         # pylint: disable=unused-argument
         return self
 
     def transform(self, texts: Iterable[str]):
-        """
-        Return the documents into a numpy array of vectors
-        """
+        """ Return the documents into a numpy array of vectors """
         if not thinc.extra.load_nlp.VECTORS:
             spacy.load(self.model_name)
         return np.array([self.transform_sentence(t) for t in texts])
