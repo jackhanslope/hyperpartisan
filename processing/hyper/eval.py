d41311eba62d2028a1a2efd8efd5a3453a101790
Jack Hanslope <code@jackhanslope.com>
2021-01-21 11:48:15 +0000

    Update hyper eval function
    Now it uses cross_validate. I can get all of the values I want by only
    running the expensive function once.

--- processing/hyper/eval.py
+++ processing/hyper/eval.py
@@ -5,7 +5,7 @@ Functions to evaluate an algorithm
   validation on it
 """
 import pandas as pd
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_validate
 
 
 def evaluate_algorithm(
@@ -14,35 +14,37 @@ def evaluate_algorithm(
     algorithm,
     n_jobs=-1,
     verbosity=3,
-    scoring="accuracy",
 ):
     """
     Return the cross val score for the given algorithm with 5 fold stratified
     cv.
     """
-    return cross_val_score(
+    return cross_validate(
         # the cv is (implicityly) stratified
         algorithm,
         X,
         y,
-        scoring=scoring,
+        scoring=["accuracy", "precision", "recall", "f1"],
         n_jobs=n_jobs,
         verbose=verbosity,
     )
 
 
-def report_algorithm(X: pd.DataFrame, y, algorithm):
+def report_algorithm(
+    X: pd.DataFrame,
+    y,
+    algorithm,
+    n_jobs=-1,
+    verbosity=0,
+):
     """
     Return mean and standard deviation for 4 metrics.
 
     """
-    acc = evaluate_algorithm(X, y, algorithm, verbosity=0, scoring="accuracy")
-    prec = evaluate_algorithm(X, y, algorithm, verbosity=0, scoring="precision")
-    rec = evaluate_algorithm(X, y, algorithm, verbosity=0, scoring="recall")
-    f1 = evaluate_algorithm(X, y, algorithm, verbosity=0, scoring="f1")
+    res = evaluate_algorithm(X, y, algorithm, n_jobs=n_jobs, verbosity=verbosity)
     return {
-        "acc": [acc.mean(), acc.std()],
-        "prec": [prec.mean(), prec.std()],
-        "rec": [rec.mean(), rec.std()],
-        "f1": [f1.mean(), f1.std()],
+        "acc": [res["test_accuracy"].mean(), res["test_accuracy"].std()],
+        "prec": [res["test_precision"].mean(), res["test_precision"].std()],
+        "rec": [res["test_recall"].mean(), res["test_recall"].std()],
+        "f1": [res["test_f1"].mean(), res["test_f1"].std()],
     }
