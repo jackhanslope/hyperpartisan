f26abc5797fbc1c2456f828252eb8a63b5a0c1e2
Jack Hanslope <code@jackhanslope.com>
2021-01-21 18:17:37 +0000

    Add docstrings
    Also renamed a couple of variables to conform with pylint.

--- processing/hyper/transformers.py
+++ processing/hyper/transformers.py
@@ -1,3 +1,6 @@
+"""
+A collection of sklearn compliant transformers
+"""
 from typing import Iterable
 
 import numpy as np
@@ -7,17 +10,31 @@ from sklearn.base import BaseEstimator, TransformerMixin
 
 
 class SpacyTransformer(BaseEstimator, TransformerMixin):
+    """
+    A transformer based off spacy models
+    """
+
     def __init__(self, model, model_name):
         self.model = model
         self.model_name = model_name
 
     def transform_sentence(self, text: str) -> np.array:
+        """
+        Return the vector representation of `text`
+        """
         return self.model(text).vector
 
-    def fit(self, X, y=None):
+    def fit(self, features, target=None):
+        """
+        Required function
+        """
+        # pylint: disable=unused-argument
         return self
 
     def transform(self, texts: Iterable[str]):
+        """
+        Return the documents into a numpy array of vectors
+        """
         if not thinc.extra.load_nlp.VECTORS:
             self.spacy_model = spacy.load(self.model_name)
         return np.array([self.transform_sentence(t) for t in texts])
