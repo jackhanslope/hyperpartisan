4ac2bd7cc98d3bd3ae24d80ea9130ef998966b48
Jack Hanslope <code@jackhanslope.com>
2021-01-22 17:02:38 +0000

    Update install_deps to use .venv

--- processing/install_deps.sh
+++ processing/install_deps.sh
@@ -3,10 +3,10 @@
 if count $argv > /dev/null
     if test $argv[1] = "dev"
         echo "Installing dev"
-        pip install -r requirements/dev.txt -f https://download.pytorch.org/whl/torch_stable.html
+        .venv/bin/pip install -r requirements/dev.txt -f https://download.pytorch.org/whl/torch_stable.html
     else if test $argv = "common"
         echo "Installing common"
-        pip install -r requirements/common.txt -f https://download.pytorch.org/whl/torch_stable.html
+        .venv/bin/pip install -r requirements/common.txt -f https://download.pytorch.org/whl/torch_stable.html
     else
         echo $argv[1] is not an option
     end
